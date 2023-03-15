58b01ce74a678e9fe63cdcc2f576264470d2344c
whalefffall <12011124@mail.sustech.edu.cn>
2023-03-16 01:09:04 +0800

    update script

--- process.py
+++ process.py
@@ -11,8 +11,8 @@ For simplicity, we only consider the average time, neither the standard deviatio
 import os
 root = os.getcwd()
 dir_list = os.listdir(root)
-warmUp = 5
-execute = 10
+warmUp = 2
+execute = 3
 stride = 3
 
 
@@ -20,12 +20,21 @@ stride = 3
 log_file = "1.out" # log file name
 result_file = "result.txt" # result file name
 
+# get dir list
+dirs = []
+with open("./spec_dirs", "r") as f:
+    lines = f.readlines()
+    for line in lines:
+        words = line.strip("\n").split(" ")
+        dirs.append(words[8])
+        print(words[8])
+
 with open(os.path.join(root, result_file), "w") as r:
-    for dir in dir_list:
+    for dir in dirs:
         cur_path = os.path.join(root, dir)
-        if not os.path.isdir(cur_path):
-            continue
+        
         print("Processing %s" % dir)
+        r.write("Processing %s\n" % dir)
         with open(os.path.join(cur_path, log_file), "r") as log:
             lines = log.readlines()
             c_cnt = 1 # command number
@@ -35,15 +44,23 @@ with open(os.path.join(root, result_file), "w") as r:
             usr_all = 0 # total user time
             sys_all = 0 # total sys time
             log.readline()
-            for idx in range(0, len(lines)):
+            idx = 0
+            while idx < len(lines):
                 if w_cnt < warmUp:
                     w_cnt = warmUp
-                    idx += stride * w_cnt
+                    idx += stride
+                    continue
                 if e_cnt == execute:
-                    r.write("Command %d: real %f, user %f, sys %f"% (c_cnt, real_all / execute, usr_all / execute, sys_all / execute))
-                    e_cnt = 0
+                    r.write("Command %d: real %f, user %f, sys %f\n"% (c_cnt, real_all / execute, usr_all / execute, sys_all / execute))
+                    print("Command %d: real %f, user %f, sys %f"% (c_cnt, real_all / execute, usr_all / execute, sys_all / execute))
+                    e_cnt = 1
                     c_cnt += 1
                     w_cnt = 0
+                    real_all = 0
+                    usr_all = 0 
+                    sys_all = 0 
+                    idx += 1
+                    continue
                 if lines[idx].startswith("real"):
                     real_all += float(lines[idx].split(" ")[1])
                 elif lines[idx].startswith("user"):
@@ -51,4 +68,8 @@ with open(os.path.join(root, result_file), "w") as r:
                 elif lines[idx].startswith("sys"):
                     sys_all += float(lines[idx].split(" ")[1])
                     e_cnt += 1
+                idx += 1
+            log.close()
+
+    r.close()
                     
